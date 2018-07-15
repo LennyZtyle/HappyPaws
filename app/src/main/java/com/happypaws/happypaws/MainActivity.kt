a23d7c93499ff598c2b6dbe0edbb26968fe724a3
Lenny Beltrán <1enny.beltran.s@gmail.com>
2018-07-15 00:24:54 -0500

    Adding first approach to class calling
    Main class calling logincontroller class
    to start handling events and information

--- app/src/main/java/com/happypaws/happypaws/MainActivity.kt
+++ app/src/main/java/com/happypaws/happypaws/MainActivity.kt
@@ -1,12 +1,27 @@
 package com.happypaws.happypaws
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import com.happypaws.login.LoginController
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var obj = LoginController()
+
+        button.setOnClickListener{
+            //textView.toString()
+            val textfield: EditText = findViewById(R.id.editText) as EditText
+            var tempText : String = textfield.text.toString()
+            textView.setText(obj.test(textfield.text.toString()))
+
+        }
     }
 }
